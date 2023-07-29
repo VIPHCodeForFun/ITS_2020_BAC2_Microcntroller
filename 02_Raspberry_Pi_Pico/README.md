98a6015c4fbf5de950c5adc89639f87c0fc5f53a
VIPHCodeForFun <vidmar.philipp@gmail.com>
2023-07-29 18:49:15 +0200

    update bit.h

--- 02_Raspberry_Pi_Pico/README.md
+++ 02_Raspberry_Pi_Pico/README.md
@@ -3,7 +3,7 @@ A Raspberrypi 4B (RPI) is used for programming the Pico.
 The RPI is connected to the Windows PC via SSH using VisualStudioCode with the remote extention. The SSH connection runs over the WLAN so that the RPI has internet access.
 
 # For the startup of the RPI we use the manual .
-https://github.com/VIPHCodeForFun/ITS_2020_BAC2_Microcntroller/blob/main/02_Raspberry_Pi_Pico/_datasheets/getting-started-with-pico.pdf
+https://datasheets.raspberrypi.com/pico/getting-started-with-pico.pdf
 
 ## The setup of the toolchain can be done in chapter 1 Quick Pico Setup.
 ## For setting up a new project on the RPI due to instructions getting-started-with-pico.pdf Chapter 8 Follow.
--- 02_Raspberry_Pi_Pico/VisualStudioCodeProjekt/hal/pin-hal.c
+++ 02_Raspberry_Pi_Pico/VisualStudioCodeProjekt/hal/pin-hal.c
@@ -9,10 +9,10 @@
 void setupModeIO(uint8_t pin, uint8_t mode)
 {
 	// Clear IO Reset !! maby it has to be ATOMIC
-	BIT_CLR(RESET, IO_BANK0);
-	// BIT_SET((RESET + ATOMIC_CLEAR), IO_BANK0);
+	// BIT_CLR(*RESET, IO_BANK0);
+	BIT_SET(*(RESET + ATOMIC_CLEAR), IO_BANK0);
 	// Wait till Reset is finished.
-	while (BIT_IS_SET(RESET_DONE, IO_BANK0))
+	while (BIT_IS_SET(*RESET_DONE, IO_BANK0))
 	{
 		/*Reset done. If a bit is set then a reset done signal has been returned by the peripheral.
 		This indicates that the peripheral’s registers are ready to be accessed.*/
@@ -24,17 +24,17 @@ void setupModeIO(uint8_t pin, uint8_t mode)
 		// Just works for PIN0 !
 		pin = 0;
 		// GPIO0_CTRL = PIN 0 !! maby PIN x 								p.244
-		// [Resetstate= 0x1f= 0001 1111] in 4:0 FUNCSEL 					p.248
-		BIT_CLR(GPIO0_CTRL, 0);
-		BIT_CLR(GPIO0_CTRL, 1);
-		BIT_CLR(GPIO0_CTRL, 2);
-		BIT_CLR(GPIO0_CTRL, 3);
-		BIT_CLR(GPIO0_CTRL, 4);
+		// Clear [Resetstate= 0x1f= 0001 1111] in 4:0 FUNCSEL 					p.248
+		BIT_CLR(*GPIO0_CTRL, 0);
+		BIT_CLR(*GPIO0_CTRL, 1);
+		BIT_CLR(*GPIO0_CTRL, 2);
+		BIT_CLR(*GPIO0_CTRL, 3);
+		BIT_CLR(*GPIO0_CTRL, 4);
 		// Set Function Select GPIO_FUNC_SIO -> Functionselect = 5 -> 0101 	p.237
-		BIT_SET(GPIO0_CTRL, 0);
-		BIT_SET(GPIO0_CTRL, 2);
+		BIT_SET(*GPIO0_CTRL, 0);
+		BIT_SET(*GPIO0_CTRL, 2);
 		// Output enable registers 1 for drive high/low based on GPIO_OUT	p.47
-		BIT_SET(GPIO_OE, pin);
+		BIT_SET(*GPIO_OE, pin);
 		break;
 	// case IMPUT:
 	default:
@@ -44,10 +44,10 @@ void setupModeIO(uint8_t pin, uint8_t mode)
 
 void setPin(uint8_t pin)
 {
-	BIT_SET(GPIO_OUT_SET, pin); // Perform an atomic bit-set on GPIO_OUT p.47
+	BIT_SET(*GPIO_OUT_SET, pin); // Perform an atomic bit-set on GPIO_OUT p.47
 }
 
 void clearPin(uint8_t pin)
 {
-	BIT_SET(GPIO_OUT_CLR, pin); // Perform an atomic bit-clear on GPIO_OUT p.47
+	BIT_SET(*GPIO_OUT_CLR, pin); // Perform an atomic bit-clear on GPIO_OUT p.47
 }
